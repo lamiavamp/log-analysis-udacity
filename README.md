90ea7b8e949482065fd712785a80dcb3d0e73819
lamiavamp <lamia-6-6@hotmail.com>
2018-11-13 23:34:11 +0300

    updating readme.md, aligning news-log-db.py to pycodestyle and shebanging the script

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
 # Log Analysis
-This project is created in regards to have a simple API to deal with a news database
-
-## Content
-- **news-db-api.py:** postgress API to the database
-- **log-output.txt:** sample output
-- **db-tables:** a description of the tables in the database
-
-##Dependencies
-You need to install python `psycopg2` module
-`pip3 install psycopg2`
-
-## Installation
-`$ git clone https://github.com/lamiavamp/log-analysis-udacity.git`
-`$ cd log-analysis-udacity`
-
-## Usage
-Simply run the following script
-`python3 news-db-api.py`
+This project is created to have a simple API dealing with a postgress news database that consists of a huge collection of articles. The main purpose of the project is to retrive meaningful information from the database.
 
 Running the code produces *three* outputs answering these questions:
 1. What are the most popular three articles of all time?
 2. Who are the most popular article authors of all time?
 3. On which days did more than 1% of requests lead to errors?
 
-Enjoy!
+## Getting Started
+
+## Content
+- **news-db-api.py:** postgress API to the database
+- **log-output.txt:** sample output
+- **db-tables:** a description of the tables in the database
+
+### Prerequissites
+- The code is running on python3, make sure it is installed. To install `python3` in Mac use homebrew: 
+`$ brew install python3`
+or follow [this link](https://realpython.com/installing-python) for other OS 
+- You need to install python `psycopg2` module:
+`pip3 install psycopg2`
+
+### Installation
+clone the repository using git
+`$ git clone https://github.com/lamiavamp/log-analysis-udacity.git`
+`$ cd log-analysis-udacity`
+
+### Usage
+Simply run the following script to get the results
+`python3 news-db-api.py`
+
+## Authors
+* **Lamia** - [lamiavamp](https://github.com/lamiavamp)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- news-db-api.py
+++ news-db-api.py
@@ -1,22 +1,26 @@
+#!/usr/bin/env python3
+
 import psycopg2
 import datetime
 
-#intializing database connection
+# intializing database connection
 DBNAME = "news"
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
 
-# What are the most popular three articles of all time? 
+
+# What are the most popular three articles of all time?
 def top_articles():
-    c.execute("""select title, count(path) as top 
-    from articles, log 
-    where path = concat('/article/',slug) and status='200 OK' 
-    group by title 
-    order by top desc 
+    c.execute("""select title, count(path) as top
+    from articles, log
+    where path = concat('/article/',slug)
+    and status='200 OK'
+    group by title
+    order by top desc
     limit 3;""")
     result = c.fetchall()
 
-    #printing results
+    # printing results
     print ("Top articles:")
     for row in result:
         print ("\""+row[0]+"\" -- ", row[1], "views")
@@ -24,35 +28,40 @@ def top_articles():
 
 # Who are the most popular article authors of all time?
 def pop_auths():
-    c.execute("""select name, count(path) as top 
-    from articles, log, authors 
-    where path = concat('/article/',slug) and status='200 OK' and authors.id = articles.author 
-    group by name 
-    order by top desc 
+    c.execute("""select name, count(path) as top
+    from articles, log, authors
+    where path = concat('/article/',slug)
+    and status='200 OK'
+    and authors.id = articles.author
+    group by name
+    order by top desc
     limit 4;""")
     result = c.fetchall()
 
-    #printing results
+    # printing results
     print ("Most popular authors:")
     for row in result:
         print ("\""+row[0]+"\" -- ", row[1], "views")
 
 
 # On which days did more than 1% of requests lead to errors?
-def error_rate():   
-    c.execute("""select * from(select t_requests.day, 
-    round( (t_errors.errors*100) / cast(t_requests.requests as numeric), 2) as err_rate 
-    from 
-    (select date(time) as day, 
-    sum(case when status != '200 OK' then 1 else 0 end) as errors from log group by day) as t_errors 
-    inner join 
-    (select date(time) as day, count(status) as requests from log group by day) as t_requests 
-    on 
-    t_errors.day = t_requests.day) as final_q 
+def error_rate():
+    c.execute("""select * from(select t_requests.day,
+    round((t_errors.errors*100)/cast(t_requests.requests as numeric),2)
+    as err_rate
+    from
+    (select date(time) as day,
+    sum(case when status != '200 OK' then 1 else 0 end) as errors
+    from log group by day) as t_errors
+    inner join
+    (select date(time) as day,
+    count(status) as requests from log group by day) as t_requests
+    on
+    t_errors.day = t_requests.day) as final_q
     where err_rate > 1.0;""")
     result = c.fetchall()
 
-    #printing results
+    # printing results
     print ("Days with more than 1% error rate encountered:")
     for row in result:
         print ("%s -- %.2f%%" % (row[0].strftime("%b %d, %Y"), row[1]))
